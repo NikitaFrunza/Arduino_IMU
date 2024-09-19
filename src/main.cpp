f2ce7957776fcfec4cb7dcf2109980c5c5e43b17
Nikita Frunza <nichita.frunza2001@gmail.com>
2024-09-19 12:19:43 +0200

    acceleration and roll

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
-#include <Arduino.h>
+#include "MPU9250.h"
+#include "Arduino.h"
 
-// put function declarations here:
-int myFunction(int, int);
+MPU9250 mpu;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Wire.begin();
+    delay(2000);
+
+    MPU9250Setting setting;
+    setting.accel_fs_sel = ACCEL_FS_SEL::A16G;
+    setting.gyro_fs_sel = GYRO_FS_SEL::G2000DPS;
+    setting.mag_output_bits = MAG_OUTPUT_BITS::M16BITS;
+    setting.fifo_sample_rate = FIFO_SAMPLE_RATE::SMPL_200HZ;
+    setting.gyro_fchoice = 0x03;
+    setting.gyro_dlpf_cfg = GYRO_DLPF_CFG::DLPF_41HZ;
+    setting.accel_fchoice = 0x01;
+    setting.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_45HZ;
+
+    if (!mpu.setup(0x68, setting)) {  // change to your own address
+        while (1) {
+            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+            delay(5000);
+        }
+    }
+}
+
+void print_roll_pitch_yaw() {
+    Serial.print("Yaw, Pitch, Roll: ");
+    Serial.print(mpu.getYaw(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getPitch(), 2);
+    Serial.print(", ");
+    Serial.println(mpu.getRoll(), 2);
+    Serial.print(mpu.getAccX(), 2);
+    Serial.print(",");
+    Serial.print(mpu.getAccY(), 2);
+    Serial.print(",");
+    Serial.println(mpu.getAccZ(), 2);
+    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if (mpu.update()) {
+        print_roll_pitch_yaw();
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
